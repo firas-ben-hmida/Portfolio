7575c28311d997e59a76151b8bc4195785efa37d
firasbh123 <firas.benhmida@esprit.tn>
2025-06-30 01:52:03 +0100

    Portfolio

---
+++ script.js
@@ -0,0 +1,21 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "YOUR_SERVICE_ID"; 
+  const templateID = "YOUR_TEMPLATE_ID"; 
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
